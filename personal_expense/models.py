00a0ff0f13d891bd49157898ad8e2b0aef0f335d
Shams-E-Shifat <shamseshifat@gmail.com>
2023-02-14 01:19:08 +0600

    database for expense and todo

--- personal_expense/models.py
+++ personal_expense/models.py
@@ -5,13 +5,16 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 # Create your models here.
 
 class Category(models.Model):
-    item_name = models.CharField(max_length=50)
-    description = models.CharField(max_length=100)
+    item_name = models.CharField(max_length=50, unique=True)
+    description = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return self.item_name
 
 
 class Expense(models.Model):
     item_name = models.CharField(max_length=50)
-    cost = models.DecimalField(decimal_places=2,
+    cost = models.DecimalField(decimal_places=2, max_digits=8,
             validators=[
                 MinValueValidator(1, message="Spend at least 1 taka or higher! Not 0 or negative."),
             ]
@@ -19,3 +22,6 @@ class Expense(models.Model):
     date = models.DateTimeField(auto_now_add=True)
     paid_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.item_name
--- todo/models.py
+++ todo/models.py
@@ -3,10 +3,20 @@ from django.conf import settings
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
-class Todo(models.Model):
+class TodoList(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    task = models.CharField(max_length=200)
-    creation_time = models.DateTimeField(auto_add_now=True)
+    list_name = models.CharField(max_length=200)
+    creation_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.list_name
+
+class TodoItem(models.Model):
+    item_name = models.CharField(max_length=200)
+    creation_time = models.DateTimeField(auto_now_add=True)
     clearance_time = models.DateTimeField(auto_now=True)
     status = models.BooleanField(default=False)
+    todo_list = models.ForeignKey(TodoList, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.item_name
